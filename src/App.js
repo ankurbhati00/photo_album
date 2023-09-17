b4c852cfc043bb04bf0b990cd5eaa29016bc6da4
dev.ankur <ankurbhati9027@gmail.com>
2023-09-17 22:07:48 +0530

    added navbar and album name input

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar/navbar.js";
+import Main from "./components/main/main.js";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import navbarStyle from "./Navbar.module.css";
+import logoImage from "../../assets/images/logo/logo.jpg";
+function Navbar() {
+  return (
+    <>
+      <div id={navbarStyle.header}>
+        <img id={navbarStyle.img} src={logoImage} alt="logo"></img>
+        <span id={navbarStyle.logo_text}>Photo Album</span>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
